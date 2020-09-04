e4b84e43d1ea45fb102f6583ff6078da3da87a3a
Caio Jaime <kobashijaime@gmail.com>
2020-09-04 08:06:40 -0300

    Upload route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var formiDable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', (req, res)=>{
+
+  let form = new formiDable.IncomingForm({
+
+    uploadDir: './upload',
+    keepExtensions: true 
+
+  });
+
+  form.parse(req, (err, fields, files)=>{
+
+    res.json({
+      files
+    });
+
+  });
 });
 
 module.exports = router;
